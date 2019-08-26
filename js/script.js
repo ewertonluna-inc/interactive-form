e5c41245793c48892e38cfe0e9ca7c87aa43fe10
Ewerton Luna <ewerton.al@gmail.com>
2019-08-26 17:27:16 -0300

    First features working
    HTML File:
    - Other job role input added.
    
    JS File:
    - Set focus on the first text field complete
    - "Job Role" section complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(option[value="other"]).on('click', function(e){
-    console.log("this");
+$('#name').focus();
+$('#otherJobOption').hide();
+
+
+$('#title').on('input', function(){
+    const $selectedElement = $(this).find(':selected');
+    const $otherJobOptionInput = $('#otherJobOption');
+  
+    if ($selectedElement.val() === 'other'){
+        $otherJobOptionInput.slideDown(100);
+    }
+    else{
+        $otherJobOptionInput.slideUp(100);
+    }
 });
