40f51a43de07c6cbab1b613ffca96223ef5e7c72
Ewerton Luna <ewerton.al@gmail.com>
2019-08-27 16:48:20 -0300

    Working on T-Shirt Info section features

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $('#name').focus();
 $('#otherJobOption').hide();
 
+$('#color').prepend('<option>Please, select a T-shit theme</option>');
+$('#color option').eq(0).attr('selected', '');
+
 
 $('#title').on('input', function(){
     const $selectedElement = $(this).find(':selected');
@@ -12,4 +15,14 @@ $('#title').on('input', function(){
     else{
         $otherJobOptionInput.slideUp(100);
     }
+});
+
+$('#design').on('input', function(){
+    const $defaultColorOption = $('#color option').eq(0);
+    
+    if ($('#design')
+        .find(':selected')
+        .text() === 'Select Theme'){
+            $defaultColorOption.attr('selected', 'selected');
+        }
 });
