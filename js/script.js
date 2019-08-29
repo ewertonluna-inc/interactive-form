3b2127e42f8c8dc41ccebc553f8a3cb33c6eeb73
Ewerton Luna <ewerton.al@gmail.com>
2019-08-29 12:22:10 -0300

    "Register for Activities" section completed

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ $('#bitcoin').hide();
 $('#color').prepend('<option>Please, select a T-shit theme</option>');
 $('#color').val($('#color option').eq(0).val());
 
+$('fieldset.activities').append('<p id="totalCost">Total: $<span></span><p>');
+$('#totalCost').hide();
+
+let totalCost = 0;
+
+showOnlyDefaultColorOption();
+
 function showOnlyDefaultColorOption(){
     $('#color').children().each(function(index, element){
         if ($(element).val() !== 'Please, select a T-shit theme'){
@@ -14,8 +21,16 @@ function showOnlyDefaultColorOption(){
     });
 }
 
-showOnlyDefaultColorOption();
-
+function calculateCost(element){
+    let activityCost = parseInt($(element).attr('data-cost').replace(/^\$(\d+)$/, '$1'));
+    
+    if ($(element).prop('checked')){
+        totalCost += activityCost;
+    } else{
+        totalCost -= activityCost;
+    }
+    return totalCost;
+}
 
 $('#title').on('input', function(){
     const $selectedElement = $(this).find(':selected');
@@ -56,28 +71,45 @@ $('#design').on('input', () => {
 });
 
 
+
+
 $('fieldset.activities').on('change', (e) => {
     const $activity = $(e.target);
-    const $activityDateAndTime = $activity.attr('data-day-and-time');
+    const activityDateAndTime = $activity.attr('data-day-and-time');
     const $activities = $('fieldset.activities label input');
+
+    calculateCost($activity);
+
+    if (totalCost > 0){
+        $('#totalCost span').text(totalCost);
+        $('#totalCost').show();
+    }
+    else{
+        $('#totalCost span').text('');
+        $('#totalCost').hide();
+    }
     
     $activities.each((index, element) => {
         
         if ($activity.prop('checked')){
-            // Maybe I can refactor these next two if blocks replacing them with a function
-            // that receives two jQuery objects and compares their data-day-and-time properties
-
+            /* 
+                Two Refactors TBD down here:
+                1 - Create function isSameDateAndTime(activity1, activity2)
+                2 - Create function enableOrDisableActivity(enable(boolean), activity)
+            */
             if (!($activity.is($(element))) &&
-                $activity.attr('data-day-and-time') === $(element).attr('data-day-and-time'))
+                activityDateAndTime === $(element).attr('data-day-and-time'))
             {
+                $(element).parent().css({'opacity':'0.3'});
                 $(element).attr('disabled', true);
             }
 
         } else {
 
             if (!($activity.is($(element))) &&
-                $activity.attr('data-day-and-time') === $(element).attr('data-day-and-time'))
+                activityDateAndTime === $(element).attr('data-day-and-time'))
             {
+                $(element).parent().css({'opacity':''});
                 $(element).attr('disabled', false);
             }
 
