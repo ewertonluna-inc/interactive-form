fa4dc55c697f146f90838413ee8a007c2589b878
Ewerton Luna <ewerton.al@gmail.com>
2019-08-30 23:53:43 -0300

    Project completed
    Aiming for Exceeds
    
    Extra requirement features are added to the script.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # techdegree_project_3
  Project 3 - Interactive Form
+
+ In this project, I used jQuery to enhance an interactive registration form for a fictional
+ conference called "Full Stack Conf".
+
+ First, the initial set up is done by hiding, showing and creating some elements to be used
+ when the page is functioning with javascript.
+
+ In this project, my goal is to get the "Exceeds Expectations" grade.
+ To reach this goal, besides building the basic workings of the form, I aimed
+ on solving these 3 extra topics:
+
+1. T Shirt Section: "Hide the color label and select menu until a T-shirt design is selected from the Desing menu"
+    - This topic is done on inside the #design input event handler. 
+        - More specific, on the else block.
+
+2. Conditional Error Message
+    - The #cc-num input and click event handler displays a real time alert
+        - if the credit card number is empty the message displayed will be 'Please, enter a credit card number'
+        - if the credit card number doesn't containg between 13 and 16 digits, the message displayed will be 
+        'Please, enter a number between 13 and 16 digits long'.
+    - When the button is submitted, if the name field is empty, it will display a particular message telling the
+    user to Enter at least one character.
+
+3. Real-time Error messages
+    - Already worked on it as mentioned on the previous topic, showing a message in real time to the user
+    when they're filling the credit card number field.
+
+
+The code definetly code use some refactoring, so I'll try to improve it later. But for now, it seems to be working fine.
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ $('#name').focus();
 $('#otherJobOption').hide();
 
 /* 
-Next 2 lines creates option to #color
-and sets it as default. 
+Next 3 lines set up the initial state
+of the color section and then hides it.
 */
 $('#color').prepend('<option>Please, select a T-shit theme</option>');
 $('#color').val($('#color option').eq(0).val());
+$("#colors-js-puns").hide();
 
 /* 
 Next 2 lines appends to the fieldset.activities 
@@ -53,67 +54,73 @@ function calculateCost(element){
     return totalCost;
 }
 
+/*
+addOrRemoveInvalidInputClass removes .invalidInput
+to the target if the boolean argument is true. Otherwise,
+it adds the class.
+*/
+function addOrRemoveInvalidInputClass(targetedElement, validOrNot){
+    if (validOrNot) {
+        $(targetedElement).removeClass('invalidInput');
+    } else {
+        $(targetedElement).addClass('invalidInput');
+    }
+}
 
+/*
+These next 6 functions are used to validate each
+field, returning true if it's valid and false otherwise.
+It also adds or removes 'invalidInput' class to the
+element depending on its validation.
+*/
 function isValidName(text){
     const isValid = /^.+$/.test(text);
-    if (isValid) {
-        $('#name').removeClass('invalidInput');
-    } else {
-        $('#name').addClass('invalidInput');
-    }
+    const target = $('#name');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
 function isValidEmail(text){
     const isValid = /^[^@]+@[^@.]+\.[a-z]+$/.test(text);
-    if (isValid) {
-        $('#mail').removeClass('invalidInput');
-    } else {
-        $('#mail').addClass('invalidInput');
-    }
+    const target = $('#mail');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
 function isValidActivitiesRegistrationField($activitiesField){
     const isValid = Boolean($activitiesField.find(':checked').length);
-    if (isValid) {
-        $('fieldset.activities legend').removeClass('invalidInput');
-    } else {
-        $('fieldset.activities legend').addClass('invalidInput');
-    }
+    const target = $('fieldset.activities legend');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
 function isValidCardNumber(text){
     const isValid = /^\d{13,16}$/.test(text);
-    if (isValid) {
-        $('#cc-num').removeClass('invalidInput');
-    } else {
-        $('#cc-num').addClass('invalidInput');
-    }
+    const target =  $('#cc-num');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
 function isValidZipCode(text){
     const isValid = /^\d{5}$/.test(text);
-    if (isValid) {
-        $('#zip').removeClass('invalidInput');
-    } else {
-        $('#zip').addClass('invalidInput');
-    }
+    const target = $('#zip');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
 function isValidCVV(text){
     const isValid = /^\d{3}$/.test(text);
-    if (isValid) {
-        $('#cvv').removeClass('invalidInput');
-    } else {
-        $('#cvv').addClass('invalidInput');
-    }
+    const target = $('#cvv');
+    addOrRemoveInvalidInputClass(target, isValid);
     return isValid;
 }
 
+/*
+isValidForm returns true if ALL the form is filled
+correctly and false othewise.
+It will only analyse the credit card section if the
+credit card option is selected.
+*/
 function isValidFormSubmission(){
     const nameTrueOrFalse = isValidName($('#name').val());
     const emailTrueOrFalse = isValidEmail($('#mail').val());
@@ -133,6 +140,11 @@ function isValidFormSubmission(){
     return nameTrueOrFalse && emailTrueOrFalse && activityFieldTrueOrFalse;
 }
 
+/*
+Job section input handler that slides down
+the other job field so it can be filled by 
+the user.
+*/
 $('#title').on('input', function(){
     const $selectedElement = $(this).find(':selected');
     const $otherJobOptionInput = $('#otherJobOption');
@@ -145,13 +157,21 @@ $('#title').on('input', function(){
     }
 });
 
-
+/*
+Design input handler that display T-shirt
+color accordingly to which design was selected.
+If no design is selected, color option will 
+display $('#color').val('Please, select a T-shit theme';
+*/
 $('#design').on('input', () => {
     const $designValue = $('#design').val();
     const $colorOptions = $('#color option');
+    // hides all color options so it can be updated correctly each time
+    // the event is fired.
     $colorOptions.hide();
 
     if ($designValue === 'js puns'){
+        $("#colors-js-puns").show();
         $colorOptions.each(function(){
             if ($(this).text().includes('JS Puns')){
                 $('#color').val('cornflowerblue');
@@ -159,6 +179,7 @@ $('#design').on('input', () => {
             }
         });
     } else if ($designValue === 'heart js'){
+        $("#colors-js-puns").show();
         $colorOptions.each(function(){
             if ($(this).text().includes('JS shirt')){
                 $('#color').val('tomato');
@@ -166,11 +187,18 @@ $('#design').on('input', () => {
             }
         });
     } else{
+        $("#colors-js-puns").hide();
         $('#color').val('Please, select a T-shit theme');
     }
 });
 
-
+/*
+Activities Fieldset Event handler 
+- displays the total cost of the activities,
+- if the an activity is checked, it disables other activities
+that occur at the same moment. If unchecked, enables back these 
+activities
+*/
 $('fieldset.activities').on('change', (e) => {
     const $activity = $(e.target);
     const activityDateAndTime = $activity.attr('data-day-and-time');
@@ -190,11 +218,7 @@ $('fieldset.activities').on('change', (e) => {
     $activities.each((index, element) => {
         
         if ($activity.prop('checked')){
-            /* 
-                Two refactors TBD down here:
-                1 - Create function isSameDateAndTime(activity1, activity2)
-                2 - Create function enableOrDisableActivity(enable(boolean), activity)
-            */
+            
             if (!($activity.is($(element))) &&
                 activityDateAndTime === $(element).attr('data-day-and-time'))
             {
@@ -212,12 +236,17 @@ $('fieldset.activities').on('change', (e) => {
             }
 
         }
-
     });
-
 });
 
-
+/* 
+Payment section Event handler  
+- first: hides all payment options so they can be updated 
+correctly later in the handler.
+- second: if the option selected is credit card, it will show the 
+credit fields to be filled. Otherwise, it will show the paypal field
+or the bitcoin field.
+*/
 $('#payment').on('change', () => {
     $('#credit-card').hide();
     $('#paypal').hide();
@@ -226,18 +255,72 @@ $('#payment').on('change', () => {
     optionText = $('#payment').find(':selected').text();
     optionText = optionText.toLowerCase();
 
-    optionText !== 'credit card' ? ($('#' + optionText).show()) : ($('#credit-card').show());
+    optionText === 'credit card' ? ($('#credit-card').show()) : ($('#' + optionText).show());
 });
 
-
+/*
+Submit button event handler
+- if the form isn't filled correctly, it won't be
+submitted and the borders of the invalid fields will
+become red.
+- else, the form is submitted
+*/
 $('button[type="submit"]').on('click', (e) => {
     $('.invalidInput').css('border-color', '');
     const isValidSubmission = isValidFormSubmission();
     
     if (!isValidSubmission){
         e.preventDefault();
-
         $('.invalidInput').css('border-color', 'red');
+        
+        // if nameAlert span exists, delete it
+        if ($('#nameAlert').length > 0){
+            $('#nameAlert').remove();
+        }
+
+        // if name is not valid
+            // create and display alert to the user 
+        if (!isValidName($('#name').val())){
+            let $nameLabel = $('#name').prev();
+            $nameLabel.append('<span> Please, enter at least one character</span>');
+            $nameLabel.children().attr('id', 'nameAlert');
+            $nameLabel.children().css('color', 'red');
+        }
     }
 });
 
+
+$('#cc-num').on('input click', (e) => {
+    console.log('event is fired');
+    // this if-block checks if the div.col-6 label span element
+    // exists, and if it does, removes it so it won't result
+    // in duplicates.
+    if ($('div.col-6 label span').length){
+        $('div.col-6 label span').remove();
+    }
+
+    // Creation and set up of the initial span tag
+    let text = ' Please, enter a credit card number';
+    $('div.col-6 label').append('<span><span>');
+    $('div.col-6 label span').text(text);
+    $('div.col-6 label span').css('color', 'red');
+    
+    // if there's a value inside the input
+        // set its text to an empty string
+        // if card number is not valid
+            // set the span text to warn the user
+    if ($(e.target).val()){
+        $('div.col-6 label span').text('');
+
+        if (!isValidCardNumber($(e.target).val())) {
+            text = 'Please, enter a number between 13 and 16 digits long'
+            $('div.col-6 label span').text(text);
+
+        }
+    }
+
+});
+
+$('#cc-num').on('blur', (e) => {
+    $('div.col-6 label span').remove();
+});
